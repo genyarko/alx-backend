bb543f5f929245edcb7e07221c1f4231529edcc5
genyarko <115112347+genyarko@users.noreply.github.com>
2023-10-23 14:43:06 +0000

    Create 1-simple_pagination.py

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,32 @@
+import csv
+from typing import List
+from .0-simple_helper_function import index_range  # Import the index_range function
+
+class Server:
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and page > 0, "page must be a positive integer"
+        assert isinstance(page_size, int) and page_size > 0, "page_size must be a positive integer"
+
+        dataset = self.dataset()
+        total_rows = len(dataset)
+
+        start_index, end_index = index_range(page, page_size)
+
+        if start_index >= total_rows:
+            return []
+
+        return dataset[start_index:end_index]
